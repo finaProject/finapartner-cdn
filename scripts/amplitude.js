d834f406897116520e856d635d148079f90dbd22
Daniel Salazar <daniel.salazar@finapartner.com>
2025-10-31 07:07:45 -0400

    adding minify scripts

--- scripts/amplitude.js
+++ scripts/amplitude.js
@@ -2,19 +2,19 @@
 window.addEventListener('load', () => {
   setTimeout(() => {
     const s = document.createElement('script');
-    s.src = "https://cdn.amplitude.com/script/91b92269e558bd283c9c3ba4fa2a39e9.js";
     s.defer = true;
     s.onload = () => {
       const i = setInterval(() => {
         if (window.amplitude?.getInstance) {
-          clearInterval(i);
           amplitude.getInstance().init("91b92269e558bd283c9c3ba4fa2a39e9", null, {
             fetchRemoteConfig: true,
             autocapture: true
           });
+          clearInterval(i);
         }
       }, 100);
     };
+    s.src = "https://cdn.amplitude.com/script/91b92269e558bd283c9c3ba4fa2a39e9.js"
     document.head.appendChild(s);
   }, 1500);
 });
